7064436638f3888dc94072061521485eee5b19df
Lee Michaeli <leeisr@gmail.com>
2020-09-22 21:13:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,10 +6,16 @@
    array.unshift(string)
  end
  
- def using_pop (array)
+ def using_pop(array)
    array.pop 
  end
  
- def pop_with_args (array)
+ def pop_with_args(array)
    array.pop(2)
  end
+ 
+ def using_shift(array)
+   array.shift 
+ end
+ 
+ def shift_with_args(array)
